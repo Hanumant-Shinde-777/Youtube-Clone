c8ed09383bd34a9c040a1aa3172f115e6e6f2c14
Hanumant-Shinde-777 <hanumantshinde1213@gmail.com>
2024-02-21 13:04:31 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-
+import React from 'react';
+import Header from './Header';
 import './App.css';
 
 function App() {
   return (
     <div className="app">
-     <h2> youtube clone</h2>
+{/* <h2> youtube clone</h2> */}
+     <Header />
     </div>
   );
 }
--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import MenuSharpIcon from '@mui/icons-material/MenuSharp';
 
 function Header() {
   return (
     <div className='header'>
         <h1> this is header</h1>
+        <MenuSharpIcon />
+    <img
+    src='https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/Logo_of_YouTube_%282015-2017%29.svg/753px-Logo_of_YouTube_%282015-2017%29.svg.png'
+    alt=''
+    ></img>
 
     </div>
   )
