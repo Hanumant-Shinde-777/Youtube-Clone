94d606bfc4370e99d4a5a033de2603c307cebb99
Hanumant-Shinde-777 <hanumantshinde1213@gmail.com>
2024-02-22 12:17:40 +0530

    imported files

--- src/Header.js
+++ src/Header.js
@@ -1,15 +1,34 @@
-import React from 'react'
+import React from 'react';
 import MenuSharpIcon from '@mui/icons-material/MenuSharp';
+import SearchIcon from '@mui/icons-material/Search';
+import VideoCallIcon from '@mui/icons-material/VideoCall';
+import AppsIcon from '@mui/icons-material/Apps';
+import CircleNotificationsIcon from '@mui/icons-material/CircleNotifications';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+
+
 
 function Header() {
   return (
     <div className='header'>
         <h1> this is header</h1>
         <MenuSharpIcon />
+
     <img
+    className="header__logo"
     src='https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/Logo_of_YouTube_%282015-2017%29.svg/753px-Logo_of_YouTube_%282015-2017%29.svg.png'
     alt=''
     ></img>
+
+      <input type="text"  />
+      <SearchIcon />
+      <VideoCallIcon />
+     <AppsIcon />
+      <CircleNotificationsIcon />
+      <AccountCircleIcon 
+      alt ="User"
+      src ="https://www.freeiconspng.com/uploads/account-icon-5.jpg"
+      />
 
     </div>
   )
