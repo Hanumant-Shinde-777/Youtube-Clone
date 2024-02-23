d6fb4fd2ef067bed5bc047c2223d9d52cace4197
Hanumant-Shinde-777 <hanumantshinde1213@gmail.com>
2024-02-23 11:54:49 +0530

    Run main file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from './Header';
+import Sidebar from './Sidebar';
 import './App.css';
 
 function App() {
@@ -7,6 +8,9 @@ function App() {
     <div className="app">
 {/* <h2> youtube clone</h2> */}
      <Header />
+    <div className="app__page">
+    <Sidebar />
+    </div>
     </div>
   );
 }
