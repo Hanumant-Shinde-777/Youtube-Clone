4ffd151cd8e7b24878a8cc2ddfd549794bc1bb73
Hanumant-Shinde-777 <hanumantshinde1213@gmail.com>
2024-03-05 17:58:33 +0530

    Switch error

--- src/App.js
+++ src/App.js
@@ -3,17 +3,28 @@ import "./App.css";
 import Header from './Header';
 import Sidebar from './Sidebar';
 import RecommendedVideos from './RecommendedVideos';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
-{/* <h2> youtube clone</h2> */}
-     <Header />
+    <Router>
+      <Switch>
+        <Route path ="/search">
+          <h1>Search page</h1>
+        </Route>
+
+        <Route path ="/">
+          <h1>Home page</h1>
+        </Route>
+
+      </Switch>
+    </Router>
+     {/* <Header />
     <div className="app__page">
     <Sidebar />
     <RecommendedVideos />
-    </div>
+    </div> */}
     </div>
   );
 }
