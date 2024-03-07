65ba667e4ee7ac841e2449839043c52ec4b46b58
Hanumant-Shinde-777 <hanumantshinde1213@gmail.com>
2024-03-07 13:16:25 +0530

    Created SearchPage file and use css

---
+++ src/SearchPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import TuneOutlinedIcon from '@mui/icons-material/TuneOutlined';
+import './SearchPage.css';
+function SearchPage() {
+  return (
+    <div className="searchPage">
+        <div className="searchPage__filter">
+            <TuneOutlinedIcon />
+            <h2>FILTER</h2>
+        </div>
+
+    </div>
+  )
+}
+
+export default SearchPage;
