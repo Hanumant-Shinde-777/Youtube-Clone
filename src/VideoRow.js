5bf0fbecd7d9e155b5d3075f067675f392ebe858
Hanumant-Shinde-777 <yogeshshinde2324@gmail.com>
2024-03-13 10:45:20 +0530

    Added view descrition

--- src/VideoRow.js
+++ src/VideoRow.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./VideoRow.css";
 
 function VideoRow({
   views,
@@ -9,7 +10,21 @@ function VideoRow({
   title,
   image,
 }) {
-  return <div></div>;
+  return (
+    <div className="videoRow">
+      <img src={image} alt="" />
+      <div className="video__text">
+        <h3> {title} </h3>
+        
+        <p>
+          {channel} . {subs} Subscribers {views} . {timestamp}
+        </p>
+        <p>
+      {description}
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default VideoRow;
